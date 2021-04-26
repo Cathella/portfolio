0dca521bc0d79d3582e9973e7abb5d0b616c7b15
Cathella <ncathie214@gmail.com>
2021-04-26 18:42:17 +0300

    style home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <section className="col-lg-6 mx-auto">
+          <nav className="d-flex justify-content-center py-4 border-bottom">
+            <a href="#" className="me-4 btn border rounded-pill px-4">
+              Home
+            </a>
+            <a href="#" className="me-4 btn border rounded-pill px-4">
+              About
+            </a>
+            <a href="#" className="btn border rounded-pill px-4">
+              Projects
+            </a>
+          </nav>
+
+          <main className="text-center mt-5">
+            <img
+              src="https://cathella.github.io/img/pp.png"
+              alt=""
+              className="rounded-pill mb-4 prof"
+            />
+            <p className="text-uppercase fw-bold fs-2 mo-space mb-5">
+              Nakitto Catherine
+            </p>
+            <p className="text-uppercase fs-5 text-muted fw-bold">
+              Full Sta<span className="border-top pt-4">ck De</span>veloper
+            </p>
+            <p className="text-muted">(Ruby on Rails | ReactJS)</p>
+          </main>
+
+          <footer className="border-top py-4 mt-5">
+            <small className="d-flex justify-content-center">
+              <a
+                href="#"
+                className="no-decoration me-4 text-muted text-uppercase"
+              >
+                Github
+              </a>
+              <a
+                href="#"
+                className="no-decoration me-4 text-muted text-uppercase"
+              >
+                Twitter
+              </a>
+              <a href="#" className="no-decoration text-muted text-uppercase">
+                LinkedIn
+              </a>
+            </small>
+          </footer>
+        </section>
+      </div>
     </div>
   );
 }
